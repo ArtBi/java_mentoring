33ca744f26c046b31cccd4e3a69a5f25f1324baa
Artem Bilozor <theartbi@gmail.com>
2020-01-26 19:12:48 +0200

    Initial commit

---
+++ src/main/java/lesson1/Factorial.java
@@ -0,0 +1,14 @@
+package lesson1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Factorial {
+
+    public static List<Integer> getFactorialList(int first, int last) {
+        List<Integer> factorialList = new ArrayList();
+        //Код пишем тут.
+
+        return factorialList;
+    }
+}
